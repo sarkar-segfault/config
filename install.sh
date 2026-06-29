d319eb7404cfc9f97bcfdd6b44ba68e167dff60f
Adhrit Sarkar <adhritsarkar@proton.me>
2026-06-29 20:18:50 +0530

    prettified install.sh output

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
+[[ -t 1 ]] && BLUE="\e[34m" || BLUE=""
+[[ -t 1 ]] && GREEN="\e[32m" || GREEN=""
+[[ -t 1 ]] && END="\e[0m" || END=""
+
 for dir in */; do
   CFGDIR="${HOME}/.config/${dir}"
 
-  echo "Removed ${CFGDIR}"
+  echo -e "${BLUE}Removing ${CFGDIR}...${END}"
   rm -rf "${HOME}/.config/${dir}"
 
-  echo "Copying ${dir} to ${CFGDIR}"
+  echo -e "${BLUE}Copying ${dir} to ${CFGDIR}...${END}"
   cp -r "${dir}" "${HOME}/.config/${dir}"
+
+  echo -e "${GREEN}Configured ${dir}!${END}"
 done
