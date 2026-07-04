d69a9f4c5c99cc2d67d904ce11bc1b19e9f96fa0
Adhrit Sarkar <adhritsarkar@proton.me>
2026-07-04 19:06:05 +0530

    changed " | " to "   " for separator

--- ironbar/status.sh
+++ ironbar/status.sh
@@ -3,6 +3,6 @@ while :; do
   BATSTAT="$(cat /sys/class/power_supply/BAT0/status)"
   BATPERC="$(cat /sys/class/power_supply/BAT0/capacity)%"
   DATETIME="$(date +'%d-%b-%Y %I:%M:%S%P')"
-  echo "${BATSTAT} ${BATPERC} | ${DATETIME}"
+  echo "${BATSTAT} ${BATPERC}   ${DATETIME}"
   sleep 1
 done
