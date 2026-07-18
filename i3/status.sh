268f02d8d6dc567d79484900389339129862bf23
Adhrit Sarkar <adhritsarkar@proton.me>
2026-07-18 18:28:56 +0530

    nound some keys and changed status

--- i3/status.sh
+++ i3/status.sh
@@ -1,9 +1,30 @@
 #!/usr/bin/env bash
+
 while :; do
   datetime=$(date +"%d-%b-%Y %H:%M:%S")
   brightness=$(brightnessctl -m | cut -d, -f4)
-  battery=$(upower -i $(upower -e | grep BAT) | awk '/percentage/ {print $2}')
-  volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%.0f%%\n", $2 * 100}')
-  echo "󰕾 ${volume} | 󰃟 ${brightness} | 󰄌 ${battery} |  ${datetime}"
+  battery_info=$(upower -i "$(upower -e | grep BAT)")
+  battery=$(awk '/percentage/ {print $2}' <<< "$battery_info")
+  battery_state=$(awk '/state/ {print $2}' <<< "$battery_info")
+  volume_info=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
+
+  if grep -q '\[MUTED\]' <<< "$volume_info"; then
+    volume_icon="󰖁"
+  else
+    volume_icon="󰕾"
+  fi
+
+  volume=$(awk '{printf "%.0f%%", $2 * 100}' <<< "$volume_info")
+
+  case "$battery_state" in
+    charging)
+      battery_icon="󰂄"
+      ;;
+    *)
+      battery_icon="󰁹"
+      ;;
+  esac
+
+  echo "${volume_icon} ${volume} | 󰃟 ${brightness} | ${battery_icon} ${battery} |  ${datetime}"
   sleep 1
 done
